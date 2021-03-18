7d5383b647c2f458af6de1e8b36823fc1c6be0cb
lsk <longsk@casc.ac.cn>
2021-03-18 17:30:51 +0800

    vue3 可以监听 修改数组下标值 和修改数组长度

--- first-vite/src/components/HelloWorld.vue
+++ first-vite/src/components/HelloWorld.vue
@@ -1,20 +1,28 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-  <div>你好啊ee5555e啊</div>
+        <h1>{{ msg }}</h1>
+        <button @click="count++">count is: {{ count }}</button>
+        <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+        <div>你好啊ee5555e啊</div>
+        <div>{{arr}}</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                count: 0,
+                arr: [1, 2, 3, 4],
+            }
+        },
+        mounted() {
+            setTimeout(() => {
+                this.arr[2] = 'lsk';
+                this.arr.length=3
+            },2000)
+        }
     }
-  }
-}
 </script>
